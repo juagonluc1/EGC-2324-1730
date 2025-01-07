1b36c12b11a6cf37f49183dec5ffe4d60502c738
Juan Antonio Gonzalez Lucena <juagonluc1@alum.us.es>
2025-01-07 22:21:26 +0100

    CAMBIO 2

--- decide/decide/urls.py
+++ decide/decide/urls.py
@@ -9,8 +9,8 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+Including anotheç URLconf
+    1. Import the include()function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
